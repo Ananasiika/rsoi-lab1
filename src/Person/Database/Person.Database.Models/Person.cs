51f0c674c0f859ee7e8f81e5db7b94d2bb1441fb
Larisa Pronina <larisa.pronina2003@gmail.com>
2025-09-22 17:04:46 +0300

    add missing file

---
+++ src/Person/Database/Person.Database.Models/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace Person.Database.Models;
+
+public class Person
+{
+    /// <summary>
+    /// Идентификатор человека.
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// Имя.
+    /// </summary>
+    public string Name { get; set; }
+    
+    /// <summary>
+    /// Возраст.
+    /// </summary>
+    public int? Age { get; set; }
+    
+    /// <summary>
+    /// Адрес.
+    /// </summary>
+    public string? Address { get; set; }
+    
+    /// <summary>
+    /// Место работы.
+    /// </summary>
+    public string? Work { get; set; }
+
+    public Person(string name, int? age, string? address, string? work)
+    {
+        Name = name;
+        Age = age;
+        Address = address;
+        Work = work;
+    }
+}
